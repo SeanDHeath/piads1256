a089ff522aa84df23b567d7a2dd458c0d804bca9
Sean Heath <seanheath87@gmail.com>
2016-03-02 10:33:52 -0500

    Ready to do initial test on Pi

---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := "ads1256-scalapi"
+
+version:= "0.1"
+
+scalaVersion := "2.11.7"
+
+sbtVersion := "0.13.11"
+
+libraryDependencies += "com.pi4j" % "pi4j-core" % "1.0" withJavadoc()
+libraryDependencies += "com.typesafe.akka" % "akka-actor_2.11" % "2.4.2"
---
+++ src/main/scala/main.scala
@@ -0,0 +1,35 @@
+import com.pi4j.io.gpio.{RaspiPin, GpioFactory}
+import com.pi4j.io.spi.{SpiMode, SpiChannel, SpiFactory}
+
+object ADS1256 {
+  def main(args: Array[String]): Unit = {
+
+    // Set up SPI
+    val speed = 1000000 // 1MHz
+    val channel = SpiChannel.getByNumber(0)
+    val mode = SpiMode.MODE_1
+
+    val spi = SpiFactory.getInstance(channel, speed, mode)
+
+    // Set up CS
+    val gpio = GpioFactory.getInstance()
+    val DRDY_PIN = gpio.provisionDigitalInputPin(RaspiPin.GPIO_00)
+    val RESET_PIN = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01)
+    val PDWN_PIN = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02)
+    val CS_PIN = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_03)
+
+    RESET_PIN.high()
+    PDWN_PIN.high()
+    CS_PIN.low()
+
+    val REG_STATUS = 0x00
+    val CMD_RREG = 0x10
+    val data = (CMD_RREG | REG_STATUS)
+
+    spi.write(data.asInstanceOf[Byte])
+
+    println(data)
+
+
+  }
+}
