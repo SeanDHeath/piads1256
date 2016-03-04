f0e55ed3c242aaf784c7e84feaa0315490aef214
Sean Heath <seanheath87@gmail.com>
2016-03-04 16:33:54 -0500

    Getting weird values to ConvertVoltage, masking off all but lower 8 bits of the Ints I pull off of spidev

--- src/main/scala/ADS1256.scala
+++ src/main/scala/ADS1256.scala
@@ -245,7 +245,7 @@ object ADS1256 {
 
     for (i <- Range(0,n)) {
       // Write always returns an Array of Byte, even if it's just one byte. Pull the byte out and add it to the List
-      result += spidev.write(0x00.toByte)(0).toInt
+      result += (spidev.write(0x00.toByte)(0).toInt & 0x000F)
     }
     result.toList
   }
