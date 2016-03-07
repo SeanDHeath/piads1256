93b069921160d944ec96c591fb8f7cb44b24ff6c
Sean Heath <seanheath87@gmail.com>
2016-03-07 17:06:16 -0500

    Voltage reading is working, as well as RREG command. This might be it!

--- src/main/scala/ADS1256.scala
+++ src/main/scala/ADS1256.scala
@@ -3,6 +3,7 @@ import java.util.concurrent.TimeUnit
 
 import ADS1256.Input.Input
 import ADS1256.Register.Register
+import akka.io.Tcp.WriteCommand
 import com.pi4j.io.gpio.{PinState, RaspiPin, GpioFactory}
 import com.pi4j.io.spi.{SpiMode, SpiChannel, SpiFactory}
 
@@ -15,81 +16,94 @@ object ADS1256 {
   val NUM_REGISTERS = 11
   object Register extends Enumeration {
     type Register = Value
-    val STATUS    = Value(0x00)
-    val MUX       = Value(0x01)
-    val ADCON     = Value(0x02)
-    val DRATE     = Value(0x03)
-    val IO        = Value(0x04)
-    val OFC0      = Value(0x05)
-    val OFC1      = Value(0x06)
-    val OFC2      = Value(0x07)
-    val FSC0      = Value(0x08)
-    val FSC1      = Value(0x09)
-    val FSC2      = Value(0x0A)
+    val STATUS    = Value(0x0000)
+    val MUX       = Value(0x0001)
+    val ADCON     = Value(0x0002)
+    val DRATE     = Value(0x0003)
+    val IO        = Value(0x0004)
+    val OFC0      = Value(0x0005)
+    val OFC1      = Value(0x0006)
+    val OFC2      = Value(0x0007)
+    val FSC0      = Value(0x0008)
+    val FSC1      = Value(0x0009)
+    val FSC2      = Value(0x000A)
   }
 
   // Analog Inputs
   object Input extends Enumeration {
     type Input    = Value
-    val AIN0      = Value(0x00)
-    val AIN1      = Value(0x01)
-    val AIN2      = Value(0x02)
-    val AIN3      = Value(0x03)
-    val AIN4      = Value(0x04)
-    val AIN5      = Value(0x05)
-    val AIN6      = Value(0x06)
-    val AIN7      = Value(0x07)
-    val AINCOM    = Value(0x08)
+    val AIN0      = Value(0x0000)
+    val AIN1      = Value(0x0001)
+    val AIN2      = Value(0x0002)
+    val AIN3      = Value(0x0003)
+    val AIN4      = Value(0x0004)
+    val AIN5      = Value(0x0005)
+    val AIN6      = Value(0x0006)
+    val AIN7      = Value(0x0007)
+    val AINCOM    = Value(0x0008)
   }
 
   // Commands
   object Command extends Enumeration {
     type Command = Value
-    val WAKEUP    = Value(0x00)
-    val RDATA     = Value(0x01)
-    val RDATAC    = Value(0x03)
-    val SDATAC    = Value(0x0F)
-    val RREG      = Value(0x10)
-    val WREG      = Value(0x50)
-    val SELFCAL   = Value(0xF0)
-    val SELFOCAL  = Value(0xF1)
-    val SELFGCAL  = Value(0xF2)
-    val SYSOCAL   = Value(0xF3)
-    val SYSGCAL   = Value(0xF4)
-    val SYNC      = Value(0xFC)
-    val STANDBY   = Value(0xFD)
-    val RESET     = Value(0xFE)
+    val WAKEUP    = Value(0x0000)
+    val RDATA     = Value(0x0001)
+    val RDATAC    = Value(0x0003)
+    val SDATAC    = Value(0x000F)
+    val RREG      = Value(0x0010)
+    val WREG      = Value(0x0050)
+    val SELFCAL   = Value(0x00F0)
+    val SELFOCAL  = Value(0x00F1)
+    val SELFGCAL  = Value(0x00F2)
+    val SYSOCAL   = Value(0x00F3)
+    val SYSGCAL   = Value(0x00F4)
+    val SYNC      = Value(0x00FC)
+    val STANDBY   = Value(0x00FD)
+    val RESET     = Value(0x00FE)
   }
 
   // DRATE Speeds
   object DataRate extends Enumeration {
     type DataRate = Value
-    val DRATE_30000 = Value(0xF0) // 30,000SPS (default)
-    val DRATE_15000 = Value(0xE0) // 15,000SPS
-    val DRATE_7500 = Value(0xD0) // 7,500SPS
-    val DRATE_3750 = Value(0xC0) // 3,750SPS
-    val DRATE_2000 = Value(0xB0) // 2,000SPS
-    val DRATE_1000 = Value(0xA1) // 1,000SPS
-    val DRATE_500 = Value(0x92) // 500SPS
-    val DRATE_100 = Value(0x82) // 100SPS
-    val DRATE_60 = Value(0x72) // 60SPS
-    val DRATE_50 = Value(0x63) // 50SPS
-    val DRATE_30 = Value(0x53) // 30SPS
-    val DRATE_25 = Value(0x43) // 25SPS
-    val DRATE_15 = Value(0x33) // 15SPS
-    val DRATE_10 = Value(0x20) // 10SPS
-    val DRATE_5 = Value(0x13) // 5SPS
-    val DRATE_2_5 = Value(0x03)// 2.5SPS
+    val DRATE_30000 = Value(0x00F0) // 30,000SPS (default)
+    val DRATE_15000 = Value(0x00E0) // 15,000SPS
+    val DRATE_7500 = Value(0x00D0) // 7,500SPS
+    val DRATE_3750 = Value(0x00C0) // 3,750SPS
+    val DRATE_2000 = Value(0x00B0) // 2,000SPS
+    val DRATE_1000 = Value(0x00A1) // 1,000SPS
+    val DRATE_500 = Value(0x0092) // 500SPS
+    val DRATE_100 = Value(0x0082) // 100SPS
+    val DRATE_60 = Value(0x0072) // 60SPS
+    val DRATE_50 = Value(0x0063) // 50SPS
+    val DRATE_30 = Value(0x0053) // 30SPS
+    val DRATE_25 = Value(0x0043) // 25SPS
+    val DRATE_15 = Value(0x0033) // 15SPS
+    val DRATE_10 = Value(0x0020) // 10SPS
+    val DRATE_5 = Value(0x0013) // 5SPS
+    val DRATE_2_5 = Value(0x0003)// 2.5SPS
   }
-  // Gain levels
-  object GainLevel extends Enumeration {
-    val AD_GAIN_1 = Value(0x00)// default
-    val AD_GAIN_2 = Value(0x01)
-    val AD_GAIN_4 = Value(0x02)
-    val AD_GAIN_8 = Value(0x03)
-    val AD_GAIN_16 = Value(0x04)
-    val AD_GAIN_32 = Value(0x05)
-    val AD_GAIN_64 = Value(0x06)
+
+  object Status extends Enumeration {
+    type Status = Value
+    val ORDER_LSB = Value(1 << 3)
+    val ACAL_ON = Value(1 << 2)
+    val BUFFER_ON = Value(1 << 1)
+  }
+
+  object ADControl extends Enumeration {
+    type ADControl = Value
+    val CLK_FCLKIN = Value(1 << 5)
+    val CLK_HALF_FCLKIN = Value(2 << 5)
+    val CLK_FOURTH_FCLKIN = Value(3 << 5)
+    val SD_500nA = Value(1 << 3)
+    val SD_2uA = Value(2 << 3)
+    val SD_10uA = Value(3 << 3)
+    val GAIN_2 = Value(0x0001)
+    val GAIN_4 = Value(0x0002)
+    val GAIN_8 = Value(0x0003)
+    val GAIN_16 = Value(0x0004)
+    val GAIN_32 = Value(0x0005)
+    val GAIN_64 = Value(0x0006)
   }
 
   // Defaults for RPi 2
@@ -98,6 +112,7 @@ object ADS1256 {
   var spi_channel = SpiChannel.getByNumber(0)
   var spi_mode = SpiMode.MODE_1
   var drdy_timeout = 500 // ms
+  var adc_gain = 1 // to adjust this, write to the
 
   // Open the new device
   val spidev = SpiFactory.getInstance(spi_channel, spi_frequency, spi_mode)
@@ -111,8 +126,14 @@ object ADS1256 {
 
   // These must be used to tell the ADS1256 we're talking to it - if we don't use them then we will get no information
   // from the chip
-  def chip_select() = CS_PIN.low()
-  def chip_release() = CS_PIN.high()
+  def chip_select() = {
+    CS_PIN.low()
+    TimeUnit.MICROSECONDS.sleep(5)
+  }
+  def chip_release() = {
+    TimeUnit.MICROSECONDS.sleep(1)
+    CS_PIN.high()
+  }
 
   // This stores the current register the ADS1256 is reading
   var current_positive_input = Input.AIN0
@@ -125,11 +146,17 @@ object ADS1256 {
 
   // Methods
   def DataReady(): Boolean = {
-    val currentTime = System.currentTimeMillis()
+    val startTime = System.currentTimeMillis()
+    var currentTime = System.currentTimeMillis()
+
+    println("DataReady: " + currentTime + ", " + startTime)
+    println(DRDY_PIN.getState)
 
     // Wait for drdy to go low or for timeout to occur
-    while (((currentTime - System.currentTimeMillis()) < drdy_timeout) &&
+    while (((currentTime - startTime) < drdy_timeout) &&
       (DRDY_PIN.getState != PinState.LOW)) {
+      currentTime = System.currentTimeMillis()
+      println("waiting for data...")
     }
 
     // If we timed out
@@ -156,8 +183,11 @@ object ADS1256 {
       commands += data
 
       chip_select()
-      WriteSPI(commands)
+      WriteSPI(Command.WREG.id | start_register.id)
+      WriteSPI(0x00)
+      WriteSPI(data)
       chip_release()
+      TimeUnit.MICROSECONDS.sleep(1)
     } else {
       println("Write Register: Provided register address is not valid")
     }
@@ -165,40 +195,16 @@ object ADS1256 {
 
   /** Writes a string of data to the SPI bus, all Ints are truncated to Bytes
     *
-    * @param values
+    * @param value
     */
-  def WriteSPI(values: Queue[Int]) = {
-    println("WriteSPI: " + values)
-    for (value <- values){
-      spidev.write(value.toByte)
-      // Need to wait between each byte
-      TimeUnit.MICROSECONDS.sleep(10)
-    }
+  def WriteSPI(value: Int) = {
+    println("WriteSPI: " + value)
+    spidev.write(value.toByte)
   }
-  def ReadSPI(n: Int): List[Byte] = {
-    val arr = Array.fill[Byte](n)(0)
-    spidev.write(arr, 0, arr.length).toList
-  }
-
-  /** Writes data to multiple consecutive registers on the ADS1256.
-    *
-    * @param start_register: Value from the Register map
-    * @param data
-    */
-  def WriteRegisters(start_register: Register, data: List[Int]) = {
-      var commands = Queue[Int]()
-      // Generate the command for the appropriate register
-      commands += (Command.WREG.id | start_register.id)
-      // Add the number of registers to write
-      commands += data.length
-      // Add the data to write
-      for (item <- data){
-        commands += item
-      }
-
-      chip_select()
-      WriteSPI(commands)
-      chip_release()
+  def ReadSPI(n: Int) = {
+    val arr = Array.fill[Byte](n)(0xFF.toByte)
+    val result = spidev.write(arr, 0, arr.length).toList
+    result.map{i: Byte => i.toInt & 0x00FF}
   }
 
   // The chip default is a differential reading between AIN0 and AIN1, this defaults to reading a single ended read
@@ -215,15 +221,46 @@ object ADS1256 {
   def SelectInput(positive_input: Input, negative_input: Input = Input.AINCOM) = {
       // Wait for DRDY before sending MUX signal
       if (DataReady()){
-        var commands = Queue[Int]()
-        commands += ((positive_input.id << 4) | (negative_input.id & 0x0F))
-        commands += Command.SYNC.id
-        commands += Command.WAKEUP.id
-
         chip_select()
-        WriteSPI(commands)
+        WriteRegister(Register.MUX, (positive_input.id << 4) | negative_input.id)
+        Sync()
+        Wakeup()
         chip_release()
       }
+  }
+
+  def Wakeup() = {
+    WriteSPI(Command.WAKEUP.id)
+  }
+
+  def Sync() = {
+    WriteSPI(Command.SYNC.id)
+    TimeUnit.MICROSECONDS.sleep(4)
+  }
+
+  def WriteRegister(register: Register, value: Int) = {
+    WriteSPI(Command.WREG.id | register.id)
+    WriteSPI(0x00)
+    WriteSPI(value)
+    TimeUnit.MICROSECONDS.sleep(1)
+  }
+
+  def ReadRegister(register: Register) = {
+    WriteSPI(Command.RREG.id | register.id)
+    WriteSPI(0x00)
+    DataDelay()
+    val value = ReadSPI(1)
+    TimeUnit.MICROSECONDS.sleep(1)
+    value
+  }
+
+  def DataDelay() = {
+    TimeUnit.MICROSECONDS.sleep(10)
+  }
+
+  def SelfCal() = {
+    WriteSPI(Command.SELFCAL.id)
+    DataReady()
   }
 
   /** Returns an Int containing the 24 bit analog voltage value provided by the ADS1256
@@ -233,26 +270,24 @@ object ADS1256 {
     // If data is ready
     if (DataReady()) {
       // Send the command
-      var commands = Queue[Int]()
-      commands += Command.RDATA.id
       chip_select()
-      WriteSPI(commands)
-      DataDelay()
+      WriteSPI(Command.RDATA.id)
+      TimeUnit.MICROSECONDS.sleep(10)
       val bytes = ReadSPI(3)
       chip_release()
+
       println(bytes)
-      val retval = (bytes(0) << 16) | (bytes(1) << 8) | (bytes(2)) & 0x0FFF
+      val msb = bytes(0)<< 24
+      val midb = bytes(1)<< 16
+      val lsb = bytes(2)<< 8
+      val retval = (msb | midb | lsb) >> 8
+      println(retval)
       Some(retval)
     } else {
       println("ReadData: Failed to read data")
       None
     }
   }
-
-  def DataDelay() = {
-    TimeUnit.MICROSECONDS.sleep(100)
-  }
-
 
   // TODO: ReadDifferentialInput
   def ReadInput(positive_input: Input, negative_input: Input = Input.AINCOM) = {
@@ -265,26 +300,17 @@ object ADS1256 {
     ReadData()
   }
 
-  def ConvertVoltage(voltage: Int, vref: Double = 5.0): Double = {
-    val resolution: Double = vref / 0x0FFF.toDouble
+  def ConvertVoltage(voltage: Int, vref: Double = 5): Double = {
+    val resolution = (vref * 2) / 0x00FFFFFF.toDouble
     voltage * resolution
   }
 
   def InitializeADS1256() = {
     var commands = Queue[Int]()
-    commands += Command.WREG.id | Register.STATUS.id
-    commands += 0x00
-    commands += 0x04 // Auto calibration enabled
     chip_select()
-    WriteSPI(commands)
-    chip_release()
-
-    commands.clear()
-    commands += Command.WREG.id | Register.DRATE.id
-    commands += 0x00
-    commands += DataRate.DRATE_1000.id // 1k samples / sec
-    chip_select()
-    WriteSPI(commands)
+    WriteRegister(Register.STATUS, Status.ACAL_ON.id)
+    WriteRegister(Register.DRATE, DataRate.DRATE_1000.id)
+    println("Read Register: " + ReadRegister(Register.STATUS))
     chip_release()
   }
 
